6f5ab25a82d2459ac499dd817519d93b967cd1bd
Mathias Nørregaard <mathias.norregaard@creativefabrica.com>
2025-12-01 22:06:11 +0100

    fix

--- src/main/java/com/github/ars_zero/client/gui/validators/SpellPhaseValidator.java
+++ src/main/java/com/github/ars_zero/client/gui/validators/SpellPhaseValidator.java
@@ -1,14 +1,8 @@
 package com.github.ars_zero.client.gui.validators;
 
-<<<<<<< HEAD
-import com.github.ars_zero.client.gui.AbstractMultiPhaseCastDeviceScreen;
-import com.github.ars_zero.common.glyph.AnchorEffect;
-import com.github.ars_zero.common.glyph.TemporalContextForm;
-=======
 import com.github.ars_zero.common.glyph.AnchorEffect;
 import com.github.ars_zero.common.glyph.TemporalContextForm;
 import com.github.ars_zero.common.spell.SpellPhase;
->>>>>>> 4e707db5d1116aed23d09488f1991c81eaf936ba
 import com.hollingsworth.arsnouveau.api.spell.AbstractSpellPart;
 import com.hollingsworth.arsnouveau.api.spell.SpellValidationError;
 import com.hollingsworth.arsnouveau.common.spell.validation.BaseSpellValidationError;
@@ -37,33 +31,22 @@ public class SpellPhaseValidator extends ScanningSpellValidator<SpellPhase> {
     private final SpellPhase phase;
     
     public SpellPhaseValidator(SpellPhase phase) {
->>>>>>> 4e707db5d1116aed23d09488f1991c81eaf936ba
         this.phase = phase;
     }
     
     @Override
-
     protected SpellPhase initContext() {
->>>>>>> 4e707db5d1116aed23d09488f1991c81eaf936ba
         return phase;
     }
     
     @Override
     protected void digestSpellPart(
-<<<<<<< HEAD
-            AbstractMultiPhaseCastDeviceScreen.DevicePhase ctx,
-=======
             SpellPhase ctx,
->>>>>>> 4e707db5d1116aed23d09488f1991c81eaf936ba
             int position,
             AbstractSpellPart part,
             List<SpellValidationError> errors
     ) {
-<<<<<<< HEAD
-        EnumSet<AbstractMultiPhaseCastDeviceScreen.DevicePhase> allowed = ALLOWED_PHASES.get(part.getClass());
-=======
         EnumSet<SpellPhase> allowed = ALLOWED_PHASES.get(part.getClass());
->>>>>>> 4e707db5d1116aed23d09488f1991c81eaf936ba
         
         if (allowed != null && !allowed.contains(ctx)) {
             errors.add(new PhaseRestrictionValidationError(position, part, "phase_restriction"));
